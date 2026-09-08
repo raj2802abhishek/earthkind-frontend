16e9d3fb6a5beabf81f1d274633779cacee86951
Huzaifa1111 <muhammad18349@gmail.com>
2026-09-09 01:26:14 +0500

    Update desktop responsive area container padding and max-width for Cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -123,10 +123,11 @@ function Cart() {
     <div
       className="cart-page-container"
       style={{
-        padding: "20px",
+        padding: "30px 40px 80px 40px",
         width: "100%",
-        maxWidth: "1760px",
+        maxWidth: "1200px",
         margin: "0 auto",
+        boxSizing: "border-box"
       }}
     >
       <h1
